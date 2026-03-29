870451fb37e42e8c6726dc293c3b6781f8cae17d
Lisa <86305405+LisaExists@users.noreply.github.com>
2026-03-29 12:40:54 +0200

    Fix extraction and moving of clang toolchain files

--- build.sh
+++ build.sh
@@ -48,8 +48,8 @@ if [ ! -d "${HOME}/toolchains/clang-r584948" ]; then
     echo -e "\n[INFO] Cloning clang-r584948 Toolchain\n"
     mkdir -p "${HOME}/toolchains/clang-r584948" && cd "${HOME}/toolchains/clang-r584948"
     curl -LO "https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/mirror-goog-main-llvm-toolchain-source/clang-r584948.tar.gz"
-    tar -xf linux-x86-refs_heads_mirror-goog-main-llvm-toolchain-source-clang-r584948.tar.gz && rm linux-x86-refs_heads_mirror-goog-main-llvm-toolchain-source-clang-r584948.tar.gz
-    mv linux-x86-refs_heads_mirror-goog-main-llvm-toolchain-source-clang-r584948 clang-r584948
+    mkdir clang-r584948
+    tar -xf linux-x86-refs_heads_mirror-goog-main-llvm-toolchain-source-clang-r584948.tar.gz - C clang-r584948 && rm linux-x86-refs_heads_mirror-goog-main-llvm-toolchain-source-clang-r584948.tar.gz
     mv clang-r584948/* ${HOME}/toolchains/clang-r584948
     cd "${KERNEL_ROOT}"
 fi
