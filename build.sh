2182b80f2d1e5dfbd6c8437362abcecbd974e48b
Lisa <86305405+LisaExists@users.noreply.github.com>
2026-03-29 13:21:58 +0200

    Fix URLs for downloading clang-r584948 toolchain

--- build.sh
+++ build.sh
@@ -48,8 +48,8 @@ if [ ! -d "${HOME}/toolchains/clang-r584948" ]; then
     echo -e "\n[INFO] Cloning clang-r584948 Toolchain\n"
     mkdir -p "${HOME}/toolchains/clang-r584948" && cd "${HOME}/toolchains/clang-r584948"
     mkdir clang-r584948 && cd clang-r584948
-    curl -LO "https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/mirror-goog-main-llvm-toolchain-source/clang-r584948.tar.gz"
-    tar -xf linux-x86-refs_heads_mirror-goog-main-llvm-toolchain-source-clang-r584948.tar.gz && rm linux-x86-refs_heads_mirror-goog-main-llvm-toolchain-source-clang-r584948.tar.gz
+    curl -LO "https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/mirror-goog-main-llvm-toolchain-source/clang-r584948.tar.gz"
+    tar -xf linux-x86-mirror-goog-main-llvm-toolchain-source-clang-r584948.tar.gz && rm linux-x86-mirror-goog-main-llvm-toolchain-source-clang-r584948.tar.gz
     cd ..
     mv clang-r584948/* ${HOME}/toolchains/clang-r584948
     cd "${KERNEL_ROOT}"
