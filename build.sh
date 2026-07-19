2ac96b5c27c4c55bceaf94a9c599bc4926523ea1
Lisa <86305405+LisaExists@users.noreply.github.com>
2026-07-19 20:45:23 +0200

    Change kernel setup script to use stable branch
    Updated the kernel setup script to use the stable branch.

--- build.sh
+++ build.sh
@@ -93,7 +93,7 @@ export BUILD_OPTIONS=(
 )
 
 build_kernel(){
-    curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -
+    curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s stable
     # Make default configuration.
     make "${BUILD_OPTIONS[@]}" gki_defconfig
 
